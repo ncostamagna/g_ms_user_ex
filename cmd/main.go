a8d2b89b5e22360907e7c393e7efb921f956a563
Nahuel <nlcostamagna@gmail.com>
2022-10-17 22:23:40 -0300

    add docker compose and listen in main

--- cmd/main.go
+++ cmd/main.go
@@ -28,10 +28,10 @@ func main() {
 	userSrv := user.NewService(l, userRepo)
 	h := handler.NewUserHTTPServer(ctx, user.MakeEndpoints(userSrv))
 	port := os.Getenv("PORT")
-
+	address := fmt.Sprintf("127.0.0.1:%s", port)
 	srv := &http.Server{
 		Handler:      accessControl(h),
-		Addr:         fmt.Sprintf("127.0.0.1:%s", port),
+		Addr:         address,
 		WriteTimeout: 10 * time.Second,
 		ReadTimeout:  4 * time.Second,
 	}
@@ -39,6 +39,7 @@ func main() {
 	errCh := make(chan error)
 
 	go func() {
+		l.Println("listen in ", address)
 		errCh <- srv.ListenAndServe()
 	}()
 
@@ -52,7 +53,7 @@ func main() {
 func accessControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEAD")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, HEAD")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With")
 
 		if r.Method == "OPTIONS" {
